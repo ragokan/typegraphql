0c23e5302762d18edce9008ab5d400b4d180023b
Rago <63511519+ragokan@users.noreply.github.com>
2021-01-18 15:24:29 +0300

    add redis

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,31 @@ import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { RegisterResolver } from "./modules/user/Register";
 import * as ground from "graphql-playground-middleware-express";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import { redis } from "./redis";
 
 (async () => {
+  const RedisStore = connectRedis(session);
+
   const app = express();
   app.use(cors());
+  app.use(
+    session({
+      store: new RedisStore({
+        client: redis,
+      }),
+      name: "qid",
+      secret: "aslkdfjoiq12312",
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 1000 * 60 * 60 * 24 * 7 * 365, // 7 years
+      },
+    })
+  );
 
   const options = await getConnectionOptions(process.env.NODE_ENV || "development");
   await createConnection({ ...options, name: "default", logging: false });

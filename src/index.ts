cd3459d2412756eac21ecb8f424caf1c0f095db0
Rago <63511519+ragokan@users.noreply.github.com>
2021-01-18 15:07:01 +0300

    add playground

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import "reflect-metadata";
 import { createConnection, getConnectionOptions } from "typeorm";
 import express from "express";
+import cors from "cors";
 import { ApolloServer } from "apollo-server-express";
 import { buildSchema } from "type-graphql";
 import { RegisterResolver } from "./modules/user/Register";
-import cors from "cors";
+import * as ground from "graphql-playground-middleware-express";
 
 (async () => {
   const app = express();
+  app.use(cors());
 
   const options = await getConnectionOptions(process.env.NODE_ENV || "development");
   await createConnection({ ...options, name: "default", logging: false });
@@ -19,12 +21,12 @@ import cors from "cors";
     }),
     context: ({ req, res }) => ({ req, res }),
   });
-
-  app.use(cors());
-  const port = process.env.PORT || 8000;
-
   apolloServer.applyMiddleware({ app });
+
+  app.get("/playground", ground.default({ endpoint: "/graphql" }));
+
+  const port = process.env.PORT || 8000;
   app.listen(port, () => {
-    console.log(`Server started at http://localhost:${port}/graphql`);
+    console.log(`Server started at http://localhost:${port}/playground`);
   });
 })();

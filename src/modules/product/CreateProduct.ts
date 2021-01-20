9a18080365c98b09e2f4c6df2066469f8ee74d82
Rago <63511519+ragokan@users.noreply.github.com>
2021-01-20 17:37:23 +0300

    add repo

--- src/modules/product/CreateProduct.ts
+++ src/modules/product/CreateProduct.ts
@@ -1,6 +1,8 @@
 import { Arg, Mutation, Resolver } from "type-graphql";
 import { ProductInput } from "../../validation/ProductValidation";
 import { Product } from "../../entity/Product";
+import { getCustomRepository } from "typeorm";
+import { ProductRepo } from "../repos/ProductRepo";
 
 @Resolver()
 export class CreateProductResolver {
@@ -8,8 +10,8 @@ export class CreateProductResolver {
   async createProduct(
     @Arg("data") { name, owner }: ProductInput
   ): Promise<Product | null> {
-    const { id } = await Product.create({ name, owner: { id: owner } }).save();
-    const product = await Product.findOne(id, { relations: ["owner"] });
+    const productRepo = getCustomRepository(ProductRepo);
+    const product = await productRepo.createAndPopulate({ name, ownerId: owner });
 
     return product || null;
   }
